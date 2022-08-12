dddaa8b43026ed1eda80cef5ce1d8ed4ef1f4a6c
Parvaiz ZIAR <ziarparvaiz@gmail.com>
2022-08-12 23:39:42 +0300

    :rocket: feat(ui): fireOceanLoading component is done
    ISSUES CLOSED: #12

---
+++ src/components/Loading/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Loading.scss';
+
+function FireOceanLoading() {
+  return (
+    <div className="loading">
+      <span className="loading-span" />
+      <span className="loading-span" />
+      <span className="loading-span" />
+      <span className="loading-span" />
+    </div>
+  );
+}
+
+export default FireOceanLoading;
