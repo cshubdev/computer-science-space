083444813cbda6bab5ac983b85e38d97b5ce9d85
Parvaiz ZIAR <ziarparvaiz@gmail.com>
2022-08-12 23:55:26 +0300

    :rocket: feat(ui): circleLoading component is done
    ISSUES CLOSED: #12

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Loading.scss';
 
-function FireOceanLoading() {
+export function FireOceanLoading() {
   return (
     <div className="loading">
       <span className="loading-span" />
@@ -12,4 +12,21 @@ function FireOceanLoading() {
   );
 }
 
-export default FireOceanLoading;
+export function LoadingCircle() {
+  return (
+    <div className="sk-circle">
+      <div className="sk-circle1 sk-child" />
+      <div className="sk-circle2 sk-child" />
+      <div className="sk-circle3 sk-child" />
+      <div className="sk-circle4 sk-child" />
+      <div className="sk-circle5 sk-child" />
+      <div className="sk-circle6 sk-child" />
+      <div className="sk-circle7 sk-child" />
+      <div className="sk-circle8 sk-child" />
+      <div className="sk-circle9 sk-child" />
+      <div className="sk-circle10 sk-child" />
+      <div className="sk-circle11 sk-child" />
+      <div className="sk-circle12 sk-child" />
+    </div>
+  );
+}
