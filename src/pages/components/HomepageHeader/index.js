9e3f1b333256704096cee4a93b73a59f21d16f6d
Parvaiz ZIAR <ziarparvaiz@gmail.com>
2022-10-16 23:12:09 +0300

    :zap: perf(home): translate component is added

--- src/pages/components/HomepageHeader/index.js
+++ src/pages/components/HomepageHeader/index.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Translate from '@docusaurus/Translate';
 import styles from './HomepageHeader.module.scss';
 
 function HomepageHeader({ children }) {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <div className={styles.HomepageHeader}>
       <div className={styles.HomepageHeader__Header}>
         <h1 className={styles.Title}>
-          {siteConfig.title}
+          <Translate id="webTitle.Title" description="Website Title">
+            Computer Science Space
+          </Translate>
           <span>.</span>
         </h1>
-        <p className={styles.Description}>{siteConfig.tagline}</p>
+        <p className={styles.Description}>
+          <Translate
+            id="webDescription.Description"
+            description="Website Description"
+          >
+            A Computer Science portal for enthusiasts. It contains well-written,
+            well-thought-out and well-explained computer science and programming
+            articles.
+          </Translate>
+        </p>
         <div className={styles.buttons}>
           <Link className={styles.Started_BTN} to="/docs/">
-            Başlayın
+            <Translate
+              id="getStarted.GetStartedButton"
+              description="Get Started Button"
+            >
+              Get Started
+            </Translate>
           </Link>
         </div>
       </div>
